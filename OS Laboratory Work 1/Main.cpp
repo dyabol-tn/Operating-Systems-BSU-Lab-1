65fdc850cbe4e1dd9811aded5175e6a49c3110c7
Maks Stepanenko <stepanenkom156@gmail.com>
2025-09-25 00:50:47 +0300

    Add files via upload
    Realised Creator, Reporter, employee

---
+++ OS Laboratory Work 1/Main.cpp
@@ -0,0 +1,12 @@
+﻿// OS Laboratory Work 1.cpp.cpp: определяет точку входа для приложения.
+//
+
+#include "OS Laboratory Work 1.cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
